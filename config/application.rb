76411b81bbb66f45b7470a688eef83f6249e49b4
ChemistryX <starpiung@naver.com>
2020-03-09 01:29:20 +0900

    reduce prewarm interval to reduce api usage

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ Jets.application.configure do
   config.mode = "api"
 
   config.prewarm.enable = true
+  config.prewarm.rate = "2 hours"
 
   config.controllers.default_protect_from_forgery = false
 end
